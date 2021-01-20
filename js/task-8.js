bcf5d731a0c54c4821363dfb8aeb418b58cfc959
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-01-20 12:20:05 +0200

    task-8 complited

--- js/task-8.js
+++ js/task-8.js
@@ -15,3 +15,47 @@
 // Каждый следующий div после первого, должен быть шире и выше предыдущего
 // на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
+
+const createBoxRef = document.querySelector('[data-action="render"]');
+const deleteBoxRef = document.querySelector('[data-action="destroy"]');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  let box = boxes.childElementCount;
+  const allBoxes = [];
+  for (box; box < amount; box += 1) {
+    createBox();
+    allBoxes.push(box);
+  }
+}
+
+function createBox() {
+  let box = boxes.childElementCount;
+  function getRandomColor() {
+    const getRandomNumber = () => parseInt(Math.random() * 255);
+    const r = getRandomNumber();
+    const g = getRandomNumber();
+    const b = getRandomNumber();
+    const rgb = `${r},${g},${b}`;
+    return rgb;
+  }
+  const div = document.createElement('div');
+  div.style.width = `${30 + box * 10}px`;
+  div.style.height = `${30 + box * 10}px`;
+  div.style.backgroundColor = `rgb(${getRandomColor()})`;
+  boxes.appendChild(div);
+}
+
+function destroyBoxes() {
+  while (boxes.hasChildNodes()) {
+    boxes.removeChild(boxes.lastChild);
+  }
+}
+
+createBoxRef.addEventListener('click', () => {
+  createBoxes(document.querySelector('#controls').firstElementChild.value);
+});
+
+deleteBoxRef.addEventListener('click', () => {
+  destroyBoxes();
+});
